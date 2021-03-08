3fb476645fc5dbe6fa0600c5d134d0e213a0f0b8
vicwjb <vicwjb@163.com>
2021-03-08 20:09:56 +0800

    增加测试示例

---
+++ tests/DBTrans.test/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Internal;
+
+using IFoxCAD.Cad;
+
+namespace test
+{
+    public class Class1
+    {
+        [CommandMethod("dbtest")]
+        public void Dbtest()
+        {
+            using var tr = new DBTrans();
+            tr.Editor.WriteMessage("\n测试 Editor 属性是否工作！");
+            tr.Editor.WriteMessage("\n----------开始测试--------------");
+            tr.Editor.WriteMessage("\n测试document属性是否工作");
+            if (tr.Document == getdoc())
+            {
+                tr.Editor.WriteMessage("\ndocument 正常");
+            }
+            tr.Editor.WriteMessage("\n测试database属性是否工作");
+            if (tr.Database == getdb())
+            {
+                tr.Editor.WriteMessage("\ndatabase 正常");
+            }
+
+
+
+        }
+
+        public Database getdb()
+        {
+            var db = Application.DocumentManager.MdiActiveDocument.Database;
+            return db;
+        }
+
+
+        public Document getdoc()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            return doc;
+        }
+    }
+}
