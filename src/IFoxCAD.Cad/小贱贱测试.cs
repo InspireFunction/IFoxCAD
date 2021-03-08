e6b89737b7a1bb52e65450fa723405a32577b481
Administrator <Administrator@DESKTOP-831UBHH>
2021-03-08 11:40:15 +0800

    小贱贱测试

---
+++ src/IFoxCAD.Cad/小贱贱测试.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFoxCAD.Cad
+{
+    class 小贱贱测试
+    {
+    }
+}
