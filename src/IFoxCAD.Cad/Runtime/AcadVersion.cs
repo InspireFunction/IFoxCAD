2fc2bb45cc2a071fb892709b777a92ce2589a2a7
vicwjb <vicwjb@163.com>
2021-03-14 23:12:38 +0800

    搬运cad环境变量及系统设置等函数

---
+++ src/IFoxCAD.Cad/Runtime/AcadVersion.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace IFoxCAD.Cad
+{
+    /// <summary>
+    /// cad版本号类
+    /// </summary>
+    public class AcadVersion
+    {
+        /// <summary>
+        /// 主版本
+        /// </summary>
+        public int Major
+        { private set; get; }
+
+        /// <summary>
+        /// 次版本
+        /// </summary>
+        public int Minor
+        { private set; get; }
+
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public double ProgId => double.Parse($"{Major}.{Minor}");
+
+        /// <summary>
+        /// 注册表名称
+        /// </summary>
+        public string ProductName
+        { private set; get; }
+
+        /// <summary>
+        /// 注册表位置
+        /// </summary>
+        public string ProductRootKey
+        { private set; get; }
+
+        private static string _pattern = @"Autodesk\\AutoCAD\\R(\d+)\.(\d+)\\.*?";
+
+        private static List<AcadVersion> _versions;
+
+        /// <summary>
+        /// 所有安装的cad的版本号
+        /// </summary>
+        public static List<AcadVersion> Versions
+        {
+            get
+            {
+                if (_versions == null)
+                {
+                    string[] copys =
+                       Registry.LocalMachine
+                       .OpenSubKey(@"SOFTWARE\Autodesk\Hardcopy")
+                       .GetValueNames();
+                    _versions = new List<AcadVersion>();
+                    foreach (var rootkey in copys)
+                    {
+                        if (Regex.IsMatch(rootkey, _pattern))
+                        {
+                            var gs = Regex.Match(rootkey, _pattern).Groups;
+                            var ver =
+                                new AcadVersion
+                                {
+                                    ProductRootKey = rootkey,
+                                    ProductName =
+                                        Registry.LocalMachine
+                                        .OpenSubKey("SOFTWARE")
+                                        .OpenSubKey(rootkey)
+                                        .GetValue("ProductName")
+                                        .ToString(),
+
+                                    Major = int.Parse(gs[1].Value),
+                                    Minor = int.Parse(gs[2].Value),
+                                };
+
+                            _versions.Add(ver);
+                        }
+                    }
+                }
+                return _versions;
+            }
+        }
+
+        /// <summary>已打开的cad的版本号</summary>
+        /// <param name="app">已打开cad的application对象</param>
+        /// <returns>cad版本号对象</returns>
+        public static AcadVersion FromApp(object app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            string acver =
+                app.GetType()
+                    .InvokeMember(
+                    "Version",
+                    BindingFlags.GetProperty,
+                    null,
+                    app,
+                    new object[0]).ToString();
+
+            var gs = Regex.Match(acver, @"(\d+)\.(\d+).*?").Groups;
+            int major = int.Parse(gs[1].Value);
+            int minor = int.Parse(gs[2].Value);
+            foreach (var ver in Versions)
+            {
+                if (ver.Major == major && ver.Minor == minor)
+                    return ver;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 转换为字符串
+        /// </summary>
+        /// <returns>表示版本号的字符串</returns>
+        public override string ToString()
+        {
+            return
+                $"名称:{ProductName}\n版本号:{ProgId}\n注册表位置:{ProductRootKey}";
+        }
+    }
+}
---
+++ src/IFoxCAD.Cad/Runtime/Env.cs
@@ -0,0 +1,431 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.GraphicsSystem;
+using System;
+
+namespace IFoxCAD.Cad
+{
+    /// <summary>
+    /// 系统管理类
+    /// <para>封装了一些系统 osmode、cmdecho、dimblk 系统变量</para>
+    /// <para>封装了常用的 文档 编辑器 数据库等对象为静态变量</para>
+    /// <para>封装了配置页面的注册表信息获取函数</para>
+    /// </summary>
+    public static class Env
+    {
+        #region Goal
+
+        /// <summary>
+        /// 当前的数据库
+        /// </summary>
+        public static Database CurrentDatabase
+        {
+            get
+            {
+                return HostApplicationServices.WorkingDatabase;
+            }
+        }
+
+        /// <summary>
+        /// 当前文档
+        /// </summary>
+        public static Document ActiveDocument
+        {
+            get
+            {
+                return Application.DocumentManager.MdiActiveDocument;
+            }
+        }
+
+        /// <summary>
+        /// 编辑器对象
+        /// </summary>
+        public static Editor Editor
+        {
+            get
+            {
+                return ActiveDocument.Editor;
+            }
+        }
+
+        /// <summary>
+        /// 图形管理器
+        /// </summary>
+        public static Manager GsManager
+        {
+            get
+            {
+                return ActiveDocument.GraphicsManager;
+            }
+        }
+
+        #endregion Goal
+
+        #region Preferences
+
+        /// <summary>
+        /// 获取当前配置的数据
+        /// </summary>
+        /// <param name="subSectionName">小节名</param>
+        /// <param name="propertyName">数据名</param>
+        /// <returns>对象</returns>
+        public static object GetCurrentProfileProperty(string subSectionName, string propertyName)
+        {
+            UserConfigurationManager ucm = Application.UserConfigurationManager;
+            IConfigurationSection cpf = ucm.OpenCurrentProfile();
+            IConfigurationSection ss = cpf.OpenSubsection(subSectionName);
+            return ss.ReadProperty(propertyName, "");
+        }
+
+        /// <summary>
+        /// 获取对话框配置的数据
+        /// </summary>
+        /// <param name="dialog">对话框对象</param>
+        /// <returns>配置项</returns>
+        public static IConfigurationSection GetDialogSection(object dialog)
+        {
+            UserConfigurationManager ucm = Application.UserConfigurationManager;
+            IConfigurationSection ds = ucm.OpenDialogSection(dialog);
+            return ds;
+        }
+
+        /// <summary>
+        /// 获取公共配置的数据
+        /// </summary>
+        /// <param name="propertyName">数据名</param>
+        /// <returns>配置项</returns>
+        public static IConfigurationSection GetGlobalSection(string propertyName)
+        {
+            UserConfigurationManager ucm = Application.UserConfigurationManager;
+            IConfigurationSection gs = ucm.OpenGlobalSection();
+            IConfigurationSection ss = gs.OpenSubsection(propertyName);
+            return ss;
+        }
+
+        #endregion Preferences
+
+        #region Enum
+
+        /// <summary>
+        /// 命令行回显系统变量， <see langword="true"/> 为显示， <see langword="false"/> 为不显示
+        /// </summary>
+        public static bool CmdEcho
+        {
+            get
+            {
+                return (int)Application.GetSystemVariable("cmdecho") == 1;
+            }
+            set
+            {
+                Application.SetSystemVariable("cmdecho", Convert.ToInt16(value));
+            }
+        }
+
+        #region Dimblk
+
+        /// <summary>
+        /// 标注箭头类型
+        /// </summary>
+        public enum DimblkType
+        {
+            /// <summary>
+            /// 实心闭合
+            /// </summary>
+            Defult,
+
+            /// <summary>
+            /// 点
+            /// </summary>
+            Dot,
+
+            /// <summary>
+            /// 小点
+            /// </summary>
+            DotSmall,
+
+            /// <summary>
+            /// 空心点
+            /// </summary>
+            DotBlank,
+
+            /// <summary>
+            /// 原点标记
+            /// </summary>
+            Origin,
+
+            /// <summary>
+            /// 原点标记2
+            /// </summary>
+            Origin2,
+
+            /// <summary>
+            /// 打开
+            /// </summary>
+            Open,
+
+            /// <summary>
+            /// 直角
+            /// </summary>
+            Open90,
+
+            /// <summary>
+            /// 30度角
+            /// </summary>
+            Open30,
+
+            /// <summary>
+            /// 闭合
+            /// </summary>
+            Closed,
+
+            /// <summary>
+            /// 空心小点
+            /// </summary>
+            Small,
+
+            /// <summary>
+            /// 无
+            /// </summary>
+            None,
+
+            /// <summary>
+            /// 倾斜
+            /// </summary>
+            Oblique,
+
+            /// <summary>
+            /// 实心框
+            /// </summary>
+            BoxFilled,
+
+            /// <summary>
+            /// 方框
+            /// </summary>
+            BoxBlank,
+
+            /// <summary>
+            /// 空心闭合
+            /// </summary>
+            ClosedBlank,
+
+            /// <summary>
+            /// 实心基准三角形
+            /// </summary>
+            DatumFilled,
+
+            /// <summary>
+            /// 基准三角形
+            /// </summary>
+            DatumBlank,
+
+            /// <summary>
+            /// 完整标记
+            /// </summary>
+            Integral,
+
+            /// <summary>
+            /// 建筑标记
+            /// </summary>
+            ArchTick,
+        }
+
+        /// <summary>
+        /// 标注箭头属性
+        /// </summary>
+        public static DimblkType Dimblk
+        {
+            get
+            {
+                string s = (string)Application.GetSystemVariable("dimblk");
+                if (string.IsNullOrEmpty(s))
+                {
+                    return DimblkType.Defult;
+                }
+                else
+                {
+                    return s.ToEnum<DimblkType>();
+                }
+            }
+            set
+            {
+                string s = GetDimblkName(value);
+                Application.SetSystemVariable("dimblk", s);
+            }
+        }
+
+        /// <summary>
+        /// 获取标注箭头名
+        /// </summary>
+        /// <param name="dimblk">标注箭头类型</param>
+        /// <returns>箭头名</returns>
+        public static string GetDimblkName(DimblkType dimblk)
+        {
+            return
+                dimblk == DimblkType.Defult
+                ?
+                "."
+                :
+                "_" + dimblk.GetName();
+        }
+
+        /// <summary>
+        /// 获取标注箭头ID
+        /// </summary>
+        /// <param name="dimblk">标注箭头类型</param>
+        /// <returns>箭头ID</returns>
+        public static ObjectId GetDimblkId(DimblkType dimblk)
+        {
+            DimblkType oldDimblk = Dimblk;
+            Dimblk = dimblk;
+            ObjectId id = HostApplicationServices.WorkingDatabase.Dimblk;
+            Dimblk = oldDimblk;
+            return id;
+        }
+
+        #endregion Dimblk
+
+        #region OsMode
+
+        /// <summary>
+        /// 捕捉模式系统变量类型
+        /// </summary>
+        public enum OSModeType
+        {
+            /// <summary>
+            /// 无
+            /// </summary>
+            None = 0,
+
+            /// <summary>
+            /// 端点
+            /// </summary>
+            End = 1,
+
+            /// <summary>
+            /// 中点
+            /// </summary>
+            Middle = 2,
+
+            /// <summary>
+            /// 圆心
+            /// </summary>
+            Center = 4,
+
+            /// <summary>
+            /// 节点
+            /// </summary>
+            Node = 8,
+
+            /// <summary>
+            /// 象限点
+            /// </summary>
+            Quadrant = 16,
+
+            /// <summary>
+            /// 交点
+            /// </summary>
+            Intersection = 32,
+
+            /// <summary>
+            /// 插入点
+            /// </summary>
+            Insert = 64,
+
+            /// <summary>
+            /// 垂足
+            /// </summary>
+            Pedal = 128,
+
+            /// <summary>
+            /// 切点
+            /// </summary>
+            Tangent = 256,
+
+            /// <summary>
+            /// 最近点
+            /// </summary>
+            Nearest = 512,
+
+            /// <summary>
+            /// 几何中心
+            /// </summary>
+            Quick = 1024,
+
+            /// <summary>
+            /// 外观交点
+            /// </summary>
+            Appearance = 2048,
+
+            /// <summary>
+            /// 延伸
+            /// </summary>
+            Extension = 4096,
+
+            /// <summary>
+            /// 平行
+            /// </summary>
+            Parallel = 8192
+        }
+
+        /// <summary>
+        /// 捕捉模式系统变量
+        /// </summary>
+        public static OSModeType OSMode
+        {
+            get
+            {
+                return (OSModeType)Convert.ToInt16(Application.GetSystemVariable("osmode"));
+            }
+            set
+            {
+                Application.SetSystemVariable("osmode", (int)value);
+            }
+        }
+
+        /// <summary>
+        /// 追加捕捉模式
+        /// </summary>
+        /// <param name="osm1">原系统变量</param>
+        /// <param name="osm2">要追加的模式</param>
+        public static void Append(this OSModeType osm1, OSModeType osm2)
+        {
+            osm1 |= osm2;
+        }
+
+        /// <summary>
+        /// 检查系统变量的模式是否相同
+        /// </summary>
+        /// <param name="osm1">原模式</param>
+        /// <param name="osm2">要比较的模式</param>
+        /// <returns>等于要比较的模式返回 <see langword="true"/>，反之返回 <see langword="false"/></returns>
+        public static bool Check(this OSModeType osm1, OSModeType osm2)
+        {
+            return (osm1 & osm2) == osm2;
+        }
+
+        /// <summary>
+        /// 取消捕捉模式
+        /// </summary>
+        /// <param name="osm1">原模式</param>
+        /// <param name="osm2">要取消的模式</param>
+        public static void Remove(this OSModeType osm1, OSModeType osm2)
+        {
+            osm1 ^= osm2;
+        }
+
+        #endregion OsMode
+
+        private static T ToEnum<T>(this string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        private static string GetName<T>(this T value)
+        {
+            return Enum.GetName(typeof(T), value);
+        }
+
+        #endregion Enum
+    }
+}
