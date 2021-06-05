ff7b77cde155bfcc7044cedf2bb9784cd5aa8d4f
giteezxk <9063830+giteezxk@user.noreply.gitee.com>
2021-06-05 18:54:24 +0800

    移动坐标系类型枚举位置

---
+++ src/IFoxCAD.Cad/ExtensionMethod/CoordinatesystemCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IFoxCAD.Cad
+{
+    /// <summary>
+    /// 坐标系类型枚举
+    /// </summary>
+    public enum CoordinateSystemCode
+    {
+        /// <summary>
+        /// 世界坐标系
+        /// </summary>
+        Wcs = 0,
+
+        /// <summary>
+        /// 用户坐标系
+        /// </summary>
+        Ucs,
+
+        /// <summary>
+        /// 模型空间坐标系
+        /// </summary>
+        MDcs,
+
+        /// <summary>
+        /// 图纸空间坐标系
+        /// </summary>
+        PDcs
+    }
+
+}
--- src/IFoxCAD.Cad/ExtensionMethod/EditorEx.cs
+++ src/IFoxCAD.Cad/ExtensionMethod/EditorEx.cs
@@ -11,32 +11,6 @@ using System.Linq;
 namespace IFoxCAD.Cad
 {
     /// <summary>
-    /// 坐标系类型枚举
-    /// </summary>
-    public enum CoordinateSystemCode
-    {
-        /// <summary>
-        /// 世界坐标系
-        /// </summary>
-        Wcs = 0,
-
-        /// <summary>
-        /// 用户坐标系
-        /// </summary>
-        Ucs,
-
-        /// <summary>
-        /// 模型空间坐标系
-        /// </summary>
-        MDcs,
-
-        /// <summary>
-        /// 图纸空间坐标系
-        /// </summary>
-        PDcs
-    }
-
-    /// <summary>
     /// 命令行扩展类
     /// </summary>
     public static class EditorEx
