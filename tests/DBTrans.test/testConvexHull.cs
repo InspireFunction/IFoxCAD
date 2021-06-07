25abb0c951c371d0799636f8fdbb08db57f6c7c7
vicwjb <vicwjb@163.com>
2021-06-07 22:38:26 +0800

    优化关于点、向量、点集的有向面积和方向的函数

--- tests/DBTrans.test/testConvexHull.cs
+++ tests/DBTrans.test/testConvexHull.cs
@@ -17,7 +17,7 @@ namespace test
         [CommandMethod("testch")]
         public void testch()
         {
-            using var tr = new DBTrans();
+            //using var tr = new DBTrans();
             //var pts = new List<Point3d>();
             //var flag = true;
             //while (flag)
@@ -32,7 +32,7 @@ namespace test
             //    {
             //        flag = false;
             //    }
-                
+
             //}
 
             //var ptt = ConvexHull.GetConvexHull(pts);
@@ -51,10 +51,38 @@ namespace test
             //pl.Color = Color.FromColorIndex(ColorMethod.ByColor, 6);
             //tr.CurrentSpace.AddEntity(pl);
 
-            var a1 = GeometryEx.GetArea(new Point2d(0, 0), new Point2d(1, 0), new Point2d(1, 1));
+            //var a1 = GeometryEx.GetArea(new Point2d(0, 0), new Point2d(1, 0), new Point2d(1, 1));
             //var a2 = ConvexHull.cross(new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(1, 1, 0));
-            tr.Editor.WriteMessage(a1.ToString());
+            //tr.Editor.WriteMessage(a1.ToString());
             //tr.Editor.WriteMessage(a2.ToString());
+
+
+            //var vec1 = new Vector2d(1, 1);
+            //var vec2 = new Vector2d(-1, 1);
+
+            //var vec3 = vec1.GetPerpendicularVector();
+            //var vec4 = vec2.GetPerpendicularVector();
+
+            //var area1 = vec2.DotProduct(vec1.GetPerpendicularVector());
+            //var area2 = vec1.DotProduct(vec2.GetPerpendicularVector());
+
+            //var area3 = vec2.DotProduct(vec1);
+            //var area4 = vec1.DotProduct(vec2);
+
+            var area5 = GeometryEx.GetArea(new List<Point2d> { new Point2d(0, 0), new Point2d(1, 1), new Point2d(-1, 1) });
+
+            var area6 = GeometryEx.GetArea(new List<Point2d> { new Point2d(0, 0), new Point2d(-1, 1), new Point2d(1, 1) });
+            //Env.Editor.WriteMessage($"vec1 的法向量= {vec3} \n");
+            //Env.Editor.WriteMessage($"vec2 的法向量= {vec4} \n");
+
+            //Env.Editor.WriteMessage($"vec2 点乘 vec1的法向量= {area1} \n");
+            //Env.Editor.WriteMessage($"vec1 点乘 vec2的法向量= {area2} \n");
+
+            //Env.Editor.WriteMessage($"vec2 点乘 vec1= {area3} \n");
+            //Env.Editor.WriteMessage($"vec1 点乘 vec2= {area4} \n");
+
+            Env.Editor.WriteMessage($"点集的有向面积：{area5} \n");
+            Env.Editor.WriteMessage($"点集的有向面积：{area6} \n");
         }
     }
 }
