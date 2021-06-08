779311cfcf6866d9da8c0ca151e20de04ac66c5e
vicwjb <vicwjb@163.com>
2021-06-08 20:45:17 +0800

    将公开的枚举类型合并到一个文件

--- src/IFoxCAD.Cad/ExtensionMethod/GeometryEx.cs
+++ src/IFoxCAD.Cad/ExtensionMethod/GeometryEx.cs
@@ -9,51 +9,6 @@ using IFoxCAD.Linq;
 namespace IFoxCAD.Cad
 {
     /// <summary>
-    /// 方向的枚举
-    /// </summary>
-    public enum OrientationType
-    {
-        /// <summary>
-        /// 左转或逆时针
-        /// </summary>
-        CounterClockWise,
-        /// <summary>
-        /// 右转或顺时针
-        /// </summary>
-        ClockWise,
-        /// <summary>
-        /// 重合或平行
-        /// </summary>
-        Parallel
-    }
-
-    /// <summary>
-    /// 点与多边形的关系类型枚举
-    /// </summary>
-    public enum PointOnRegionType
-    {
-        /// <summary>
-        /// 多边形内部
-        /// </summary>
-        Inside,
-
-        /// <summary>
-        /// 多边形上
-        /// </summary>
-        On,
-
-        /// <summary>
-        /// 多边形外
-        /// </summary>
-        Outside,
-
-        /// <summary>
-        /// 错误
-        /// </summary>
-        Error
-    }
-
-    /// <summary>
     /// 图形扩展类
     /// </summary>
     public static class GeometryEx
