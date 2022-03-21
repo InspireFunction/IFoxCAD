bf3e0eee3c402397dc7cb0c7dd885a746cb44978
liuqihong <540762622@qq.com>
2022-03-21 11:47:12 +0800

    修改命名空间

--- src/IFoxCAD.Basal/LoopList.cs
+++ src/IFoxCAD.Basal/LoopList.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IFoxCAD.Collections
+﻿namespace IFoxCAD.Basal
 {
     /// <summary>
     /// 环链表节点
