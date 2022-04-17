3dffebd6742893c1a01e5bc973d20c582bbdda50
liuqihong <540762622@qq.com>
2022-04-17 22:00:39 +0800

    修改AOP注入事务例子

--- tests/Test/TestAOP.cs
+++ tests/Test/TestAOP.cs
@@ -28,24 +28,11 @@ public class AutoAOP
 
 namespace Test
 {
-    //拒绝注入事务,写类上,则方法全都拒绝
-    [IFoxRefuseInjectionTransaction]
-    public class MyClass2
-    {
-        //此时这个也是拒绝的..这里加特性只是无所谓
-        [IFoxRefuseInjectionTransaction]
-        [CommandMethod("IFoxRefuseInjectionTransaction2")]
-        public void TestIFoxRefuseInjectionTransaction()
-        {
-        }
-
-        [CommandMethod("InjectionTransaction2")]
-        public void InjectionTransaction()
-        {
-        }
-    }
-
-    public class MyClass
+    /* 
+     * 天秀的事务注入,让你告别事务处理
+     * https://www.cnblogs.com/JJBox/p/16157578.html
+     */
+    public class AopTestClass
     {
         //类不拒绝,这里拒绝
         [IFoxRefuseInjectionTransaction]
@@ -56,6 +43,28 @@ namespace Test
 
         //不拒绝
         [CommandMethod("InjectionTransaction")]
+        public void InjectionTransaction()
+        {
+            //怎么用事务呢?
+            //直接用 DBTrans.Top
+        }
+    }
+
+    //拒绝注入事务,写类上,则方法全都拒绝
+    [IFoxRefuseInjectionTransaction]
+    public class AopTestClassRefuseInjection
+    {
+        //此时这个也是拒绝的..这里加特性只是无所谓
+        [IFoxRefuseInjectionTransaction]
+        [CommandMethod("IFoxRefuseInjectionTransaction2")]
+        public void TestIFoxRefuseInjectionTransaction()
+        {
+            //拒绝注入就要自己开事务,通常用在循环提交事务上面.
+            //另见 报错0x02 https://www.cnblogs.com/JJBox/p/10798940.html
+            using var tr = new DBTrans();
+        }
+
+        [CommandMethod("InjectionTransaction2")]
         public void InjectionTransaction()
         {
         }
