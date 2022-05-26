d478ff88c99673bab2b4ae033835b22a0dc453dd
vicwjb <403009819@qq.com>
2022-05-27 00:02:36 +0800

    增加ValueTuple,Index,Range支持

--- tests/TestConsole/Program.cs
+++ tests/TestConsole/Program.cs
@@ -5,12 +5,18 @@ using System.ComponentModel;
 //using IFoxCAD.Basal;
 //using static TestConsole.Program;
 using TestConsole;
+using System.Runtime.CompilerServices;
 
 
+var valuetuple = (1, 2);
 
+Console.WriteLine(valuetuple.ToString());
 
-
-
-
+int[] someArray = new int[5] { 1, 2, 3, 4, 5 };
+int lastElement = someArray[^1]; // lastElement = 5
+Console.WriteLine(lastElement);
+int midElement = someArray[^3];
+Console.WriteLine(midElement);
+var range = someArray[1..3];
 
 Console.ReadLine();
