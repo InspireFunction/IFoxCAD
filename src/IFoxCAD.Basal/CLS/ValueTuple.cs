b8ab655aaab58add282df911f3d67d67e694af7c
liuqihong <540762622@qq.com>
2022-06-23 00:46:07 +0800

    修改于SR冲突

--- src/IFoxCAD.Basal/CLS/ValueTuple.cs
+++ src/IFoxCAD.Basal/CLS/ValueTuple.cs
@@ -61,7 +61,8 @@ namespace System.Numerics.Hashing
 
 namespace System
 {
-    internal static class SR
+    //internal static class SR 
+    internal sealed partial class SR
     {
         // public const string ArgumentException_ValueTupleIncorrectType = "The parameter should be a ValueTuple type of appropriate arity.";
         // public const string ArgumentException_ValueTupleLastArgumentNotAValueTuple = "The TRest type argument of ValueTuple`8 must be a ValueTuple."; 
