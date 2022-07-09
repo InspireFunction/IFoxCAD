664b15f9bdf3569f7422b83195345218d835bb7a
liuqihong <540762622@qq.com>
2022-07-09 23:41:13 +0800

    增加遍历枚举

--- tests/TestConsole/Program.cs
+++ tests/TestConsole/Program.cs
@@ -1,7 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Text;
 using IFoxCAD.Basal;
 //using static TestConsole.Program;
+
+
+/*测试遍历枚举*/
+//Season a = Season.Autumn;
+//Console.WriteLine($"Integral value of {a} is {(int)a}");  // output: Integral value of Autumn is 2
+//foreach (var enumItem in Enum.GetValues(typeof(Season)))
+//    Console.WriteLine((byte)enumItem);
+
+var sb = new StringBuilder();
+var enums = Enum.GetValues(typeof(Season)).GetEnumerator();
+while (enums.MoveNext())
+{
+    sb.Append(((byte)enums.Current).ToString());
+    sb.Append(",");
+}
+Console.WriteLine(sb);
+
+sb.Remove(sb.Length - 1, 1);//剔除末尾,
+//因为有返回值所以容易理解成 sb = sb.Remove(sb.Length - 1, 1);
+Console.WriteLine(sb);
+
 
 /*下面是元组测试*/
 var valuetuple = (1, 2);
@@ -19,3 +41,13 @@ foreach (var item in range)
     Console.WriteLine(item);
 }
 Console.ReadLine();
+
+
+public enum Season : byte
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
