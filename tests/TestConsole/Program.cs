d3d4ec80368228725883f19679ad0921e54e3499
liuqihong <540762622@qq.com>
2022-07-10 21:22:51 +0800

    增加表达式树例子

--- tests/TestConsole/Program.cs
+++ tests/TestConsole/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Text;
-using IFoxCAD.Basal;
-//using static TestConsole.Program;
 
 
-/*测试遍历枚举*/
+//表达式树例子
+TestConsole.Expression.Demo();
+
+#region 元组测试
+var valuetuple = (1, 2);
+
+Console.WriteLine(valuetuple.ToString());
+
+int[] someArray = new int[5] { 1, 2, 3, 4, 5 };
+int lastElement = someArray[^1]; // lastElement = 5
+Console.WriteLine(lastElement);
+int midElement = someArray[^3];
+Console.WriteLine(midElement);
+var range = someArray[1..3];
+foreach (var item in range)
+    Console.WriteLine(item);
+#endregion
+
+Console.ReadLine();
+
+
+#region 测试遍历枚举
 //Season a = Season.Autumn;
 //Console.WriteLine($"Integral value of {a} is {(int)a}");  // output: Integral value of Autumn is 2
 //foreach (var enumItem in Enum.GetValues(typeof(Season)))
@@ -25,25 +44,6 @@ sb.Remove(sb.Length - 1, 1);//剔除末尾,
 //因为有返回值所以容易理解成 sb = sb.Remove(sb.Length - 1, 1);
 Console.WriteLine(sb);
 
-
-/*下面是元组测试*/
-var valuetuple = (1, 2);
-
-Console.WriteLine(valuetuple.ToString());
-
-int[] someArray = new int[5] { 1, 2, 3, 4, 5 };
-int lastElement = someArray[^1]; // lastElement = 5
-Console.WriteLine(lastElement);
-int midElement = someArray[^3];
-Console.WriteLine(midElement);
-var range = someArray[1..3];
-foreach (var item in range)
-{
-    Console.WriteLine(item);
-}
-Console.ReadLine();
-
-
 public enum Season : byte
 {
     Spring,
@@ -51,4 +51,4 @@ public enum Season : byte
     Autumn,
     Winter
 }
-
+#endregion
---
+++ tests/TestConsole/表达式树.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace TestConsole
+{
+    public class Expression
+    {
+        /// <summary>
+        /// 表达式树
+        /// <a href="https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/concepts/expression-trees/">MSDN链接</a>
+        /// </summary>
+        public static void Demo()
+        {
+            // 创建表达式树  
+            Expression<Func<int, bool>> exprTree = num => num < 5;
+
+            // 分解表达式树
+            ParameterExpression param = exprTree.Parameters[0];
+            BinaryExpression operation = (BinaryExpression)exprTree.Body;//函数体
+            ParameterExpression left = (ParameterExpression)operation.Left;//左节点
+            ConstantExpression right = (ConstantExpression)operation.Right;//右表达式
+
+            Console.WriteLine("表达式树例子: {0} => {1} {2} {3}",
+                              param.Name, left.Name, operation.NodeType, right.Value);
+        }
+    }
+}
