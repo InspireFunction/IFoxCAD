66f20095d33324574ab0fdfd6cfb7a96a681111e
liuqihong <540762622@qq.com>
2022-07-19 21:07:25 +0800

    优化字符串拼接

--- src/IFoxCAD.Cad/Runtime/Log.cs
+++ src/IFoxCAD.Cad/Runtime/Log.cs
@@ -21,32 +21,25 @@ public static class Log
 
     static Log()
     {
-        try
+        //微软回复:静态构造函数只会被调用一次,
+        //并且在它执行完成之前,任何其它线程都不能创建这个类的实例或使用这个类的静态成员
+        //https://blog.csdn.net/weixin_34204722/article/details/90095812
+        var sb = new StringBuilder();
+        sb.Append(Environment.CurrentDirectory);
+        sb.Append("\\ErrorLog");
+
+        //新建文件夹
+        var path = sb.ToString();
+        if (!Directory.Exists(path))
         {
-            _logWriteLock.EnterWriteLock();
-
-            //调用静态方法的时候才会到构造函数,而且仅第一次时候才执行一次
-            var sb = new StringBuilder();
-            sb.Append(Environment.CurrentDirectory);
-            sb.Append("\\ErrorLog");
-
-            //新建文件夹
-            var path = sb.ToString();
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path)
-                         .Attributes = FileAttributes.Normal; //设置文件夹属性为普通
-            }
-
-            sb.Append('\\');
-            sb.Append(DateTime.Now.ToString("yy-MM-dd"));
-            sb.Append(".log");
-            _logAddress = sb.ToString();
+            Directory.CreateDirectory(path)
+                     .Attributes = FileAttributes.Normal; //设置文件夹属性为普通
         }
-        finally
-        {
-            _logWriteLock.ExitWriteLock();
-        }
+
+        sb.Append('\\');
+        sb.Append(DateTime.Now.ToString("yy-MM-dd"));
+        sb.Append(".log");
+        _logAddress = sb.ToString();
     }
 
 
@@ -55,14 +48,14 @@ public static class Log
     /// </summary>
     /// <param name="ex">异常</param>
     /// <param name="remarks">备注</param>
-    public static string? WriteLog(this System.Exception? ex, object? remarks = null)
+    public static string? WriteLog(this System.Exception? ex, string? remarks = null, bool printDebugWindow = true)
     {
         if (ex == null)
             return null;
 
         try
         {
-            _logWriteLock.EnterWriteLock();
+            _logWriteLock.EnterWriteLock();// 写模式锁定 读写锁
 
             var logtxt = new LogTxt(ex, remarks);
             //var logtxtJson = Newtonsoft.Json.JsonConvert.SerializeObject(logtxt, Formatting.Indented);
@@ -75,16 +68,19 @@ public static class Log
             sw.Close();
             sw.Dispose();
 #if DEBUG
-            Debug.WriteLine("错误日志: " + _logAddress);
-            Debug.Write(logtxtJson);
-            return logtxtJson;
+            if (printDebugWindow)
+            {
+                Debug.WriteLine("错误日志: " + _logAddress);
+                Debug.Write(logtxtJson);
+            }
             //Debugger.Break(); 
             //Debug.Assert(false, "终止进程");
 #endif
-        } 
+            return logtxtJson;
+        }
         finally
         {
-            _logWriteLock.ExitWriteLock();
+            _logWriteLock.ExitWriteLock();// 解锁 读写锁
         }
     }
 }
@@ -96,25 +92,21 @@ public class LogTxt
     public string 备注信息;
     public string 异常信息;
     public string 异常对象;
-    public string? 触发方法;
-    public string? 调用堆栈;
+    public string 触发方法;
+    public string 调用堆栈;
 
-    public LogTxt(System.Exception ex, object? remarks = null)
+    public LogTxt(System.Exception ex, string? remarks = null)
     {
         // 以不同语言显示日期
         // DateTime.Now.ToString("f", new System.Globalization.CultureInfo("es-ES"))
         // DateTime.Now.ToString("f", new System.Globalization.CultureInfo("zh-cn"))
         // 为了最小信息熵,所以用这样的格式,并且我喜欢补0
         this.当前时间 = DateTime.Now.ToString("yy-MM-dd hh:mm:ss");
-
-        if (remarks == null)
-            this.备注信息 = "";
-        else
-            this.备注信息 = remarks.ToString();
+        this.备注信息 = remarks ?? string.Empty;
         this.异常信息 = ex.Message;
         this.异常对象 = ex.Source;
-        this.触发方法 = ex.TargetSite?.ToString();
-        this.调用堆栈 = ex.StackTrace?.Trim();
+        this.触发方法 = ex.TargetSite == null ? string.Empty : ex.TargetSite.ToString();
+        this.调用堆栈 = ex.StackTrace == null ? string.Empty : ex.StackTrace.Trim();
     }
 
     /// 为了不引入json的dll,所以这里自己构造
@@ -123,13 +115,12 @@ public class LogTxt
         var sb = new StringBuilder();
         sb.Append('{');
         sb.Append(Environment.NewLine);
-        //为了最小信息熵,所以用这样的格式,并且我喜欢补0
-        sb.AppendLine("  \"当前时间\": \"" + 当前时间 + "\"");
-        sb.AppendLine("  \"备注信息\": \"" + 备注信息 + "\"");
-        sb.AppendLine("  \"异常信息\": \"" + 异常信息 + "\"");
-        sb.AppendLine("  \"异常对象\": \"" + 异常对象 + "\"");
-        sb.AppendLine("  \"触发方法\": \"" + 触发方法 + "\"");
-        sb.AppendLine("  \"调用堆栈\": \"" + 调用堆栈?.Trim() + "\"");
+        sb.AppendLine($"  \"{nameof(当前时间)}\": \"{当前时间}\"");
+        sb.AppendLine($"  \"{nameof(备注信息)}\": \"{备注信息}\"");
+        sb.AppendLine($"  \"{nameof(异常信息)}\": \"{异常信息}\"");
+        sb.AppendLine($"  \"{nameof(异常对象)}\": \"{异常对象}\"");
+        sb.AppendLine($"  \"{nameof(触发方法)}\": \"{触发方法}\"");
+        sb.AppendLine($"  \"{nameof(调用堆栈)}\": \"{调用堆栈}\"");
         sb.Append('}');
         return sb.ToString();
     }
