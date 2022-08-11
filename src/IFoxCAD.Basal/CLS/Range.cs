1079f4097d16dd527883cea7ff8856233775bcd0
liuqihong <540762622@qq.com>
2022-08-11 10:28:54 +0800

    优化格式

--- src/IFoxCAD.Basal/CLS/Range.cs
+++ src/IFoxCAD.Basal/CLS/Range.cs
@@ -2,9 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+namespace System;
+
 using System.Runtime.CompilerServices;
 
-namespace System;
 
 /// <summary>Represent a range has start and end indexes.</summary>
 /// <remarks>
