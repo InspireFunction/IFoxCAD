92652f7b5cad5bd5a6a44606aabbd6d5fbc1d448
liuqihong <540762622@qq.com>
2022-09-14 22:47:36 +0800

    LoopState文件统一

--- src/IFoxCAD.Cad.Shared/ExtensionMethod/Enums.cs
+++ src/IFoxCAD.Cad.Shared/ExtensionMethod/Enums.cs
@@ -172,24 +172,3 @@ public enum FontTTF
     [Description("Romans")]
     Romans
 }
-
-
-public class LoopState
-{
-    const int PLS_NONE = 0;
-    const int PLS_EXCEPTIONAL = 1;
-    const int PLS_BROKEN = 2;
-    const int PLS_STOPPED = 4;
-    const int PLS_CANCELED = 8;
-
-    private volatile int _LoopStateFlags = PLS_NONE;
-
-    public bool IsRun => _LoopStateFlags == PLS_NONE;
-    public bool IsCancel => _LoopStateFlags == PLS_CANCELED;
-    public bool IsExceptional => _LoopStateFlags == PLS_EXCEPTIONAL;
-
-    public bool IsBreak => (_LoopStateFlags & PLS_BROKEN) == PLS_BROKEN;
-    public bool IsStop => (_LoopStateFlags & PLS_STOPPED) == PLS_STOPPED;
-    public void Stop() => _LoopStateFlags = PLS_STOPPED;
-    public void Break() => _LoopStateFlags = PLS_BROKEN;
-}
