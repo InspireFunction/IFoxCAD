fa8c5d3dd10c036822154afe0a9abbad8913850e
liuqihong <540762622@qq.com>
2022-10-04 00:37:18 +0800

    瞬态图元例子增加

--- tests/TestShared/TestJigExTransient.cs
+++ tests/TestShared/TestJigExTransient.cs
@@ -16,9 +16,9 @@ public partial class Test
         var c2 = new Circle(pt.Polar(0, 300), Vector3d.ZAxis, 100);
 
         // 创建瞬态容器
-        using var jet = new JigExTransient();
+        using JigExTransient jet = new();
 
-        // 将c1以默认模式、c2以亮显模式加到瞬态容器,即在图纸上显示
+        // 将c1以默认模式,c2以亮显模式加到瞬态容器,即在图纸上显示
         jet.Add(c1);
         jet.Add(c2, Acgi.TransientDrawingMode.Highlight);
 
@@ -28,11 +28,11 @@ public partial class Test
             return;
         var pt2 = r2.Value.Ucs2Wcs();
 
-        var c3 = new Circle(pt2, Vector3d.ZAxis, 150);
-
         // 将c1从瞬态容器中移除,将c2修改颜色,c3加入瞬态容器
         jet.Remove(c1);
+
         c2.ColorIndex = 1;
+        var c3 = new Circle(pt2, Vector3d.ZAxis, 150);
         jet.Add(c3);
 
         // 由于c2进行了修改,所以需要更新,
@@ -43,11 +43,47 @@ public partial class Test
         var r4 = Env.Editor.GetPoint("\n此拾取无意义,仅为了暂停查看");
 
         // 加到图纸中,为测试瞬态容器可以自行dispose消失,所以未全部加入
-        using var tr = new DBTrans();
+        using DBTrans tr = new();
         tr.CurrentSpace.AddEntity(c3);
 
         // 若想将容器中所有图元全部加入提供了Entities属性
         // tr.CurrentSpace.AddEntity(jet.Entities);
+    }
+
+    [CommandMethod(nameof(TestJigExTransentDim))]
+    public static void TestJigExTransentDim()
+    {
+        using DBTrans tr = new();
+        Editor ed = tr.Editor!;
+        PromptPointOptions ppo = new("")
+        {
+            AppendKeywordsToMessage = false,
+        };
+        List<Point3d> pts = new();
+        for (int i = 0; i < 3; i++)
+        {
+            ppo.Message = $"\n选择标注点{i + 1}";
+            var ppr = ed.GetPoint(ppo);
+            if (ppr.Status != PromptStatus.OK)
+                return;
+            pts.Add(ppr.Value);
+        }
+
+        using RotatedDimension dimension = new(
+            rotation: 0,
+            line1Point: pts[0],
+            line2Point: pts[1],
+            dimensionLinePoint: pts[2],
+            dimensionText: "<>",
+            tr.Database.Dimstyle);
+
+        using JigExTransient jet = new();
+        jet.Add(dimension);
+        jet.UpdateAll();
+
+        var r4 = Env.Editor.GetPoint("\n此拾取无意义,仅为了暂停查看");
+
+        tr.CurrentSpace.AddEntity(dimension);
     }
 }
 #endif
