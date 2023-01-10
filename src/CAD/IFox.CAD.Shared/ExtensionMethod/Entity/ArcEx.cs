70bf14a2800fdb777196a963df5521f6a5716549
vicwjb <vicwjb@163.com>
2023-01-10 22:56:23 +0800

    拆分EntityEx扩展类，fixed #I68WMS

---
+++ src/CAD/IFox.CAD.Shared/ExtensionMethod/Entity/ArcEx.cs
@@ -0,0 +1,73 @@
+﻿namespace IFoxCAD.Cad;
+
+/// <summary>
+/// 圆弧扩展类
+/// </summary>
+public static class ArcEx
+{
+    #region 圆弧
+
+    /// <summary>
+    /// 根据圆心、起点、终点来创建圆弧(二维)
+    /// </summary>
+    /// <param name="startPoint">起点</param>
+    /// <param name="centerPoint">圆心</param>
+    /// <param name="endPoint">终点</param>
+    /// <returns>圆弧</returns>
+    public static Arc CreateArcSCE(Point3d startPoint, Point3d centerPoint, Point3d endPoint)
+    {
+        Arc arc = new();
+        arc.SetDatabaseDefaults();
+        arc.Center = centerPoint;
+        arc.Radius = centerPoint.DistanceTo(startPoint);
+        Vector2d startVector = new(startPoint.X - centerPoint.X, startPoint.Y - centerPoint.Y);
+        Vector2d endVector = new(endPoint.X - centerPoint.X, endPoint.Y - centerPoint.Y);
+        // 计算起始和终止角度
+        arc.StartAngle = startVector.Angle;
+        arc.EndAngle = endVector.Angle;
+        return arc;
+    }
+    /// <summary>
+    /// 三点法创建圆弧(二维)
+    /// </summary>
+    /// <param name="startPoint">起点</param>
+    /// <param name="pointOnArc">圆弧上的点</param>
+    /// <param name="endPoint">终点</param>
+    /// <returns>圆弧</returns>
+    public static Arc CreateArc(Point3d startPoint, Point3d pointOnArc, Point3d endPoint)
+    {
+        // 创建一个几何类的圆弧对象
+        CircularArc3d geArc = new(startPoint, pointOnArc, endPoint);
+        // 将几何类圆弧对象的圆心和半径赋值给圆弧
+#if !gcad
+#if NET35
+        return (Arc)geArc.ToCurve();
+#else
+        return (Arc)Curve.CreateFromGeCurve(geArc);
+#endif
+#else
+        return (Arc)geArc.ToCurve();
+#endif
+    }
+
+    /// <summary>
+    /// 根据起点、圆心和圆弧角度创建圆弧(二维)
+    /// </summary>
+    /// <param name="startPoint">起点</param>
+    /// <param name="centerPoint">圆心</param>
+    /// <param name="angle">圆弧角度</param>
+    /// <returns>圆弧</returns>
+    public static Arc CreateArc(Point3d startPoint, Point3d centerPoint, double angle)
+    {
+        Arc arc = new();
+        arc.SetDatabaseDefaults();
+        arc.Center = centerPoint;
+        arc.Radius = centerPoint.DistanceTo(startPoint);
+        Vector2d startVector = new(startPoint.X - centerPoint.X, startPoint.Y - centerPoint.Y);
+        arc.StartAngle = startVector.Angle;
+        arc.EndAngle = startVector.Angle + angle;
+        return arc;
+    }
+
+    #endregion
+}
---
+++ src/CAD/IFox.CAD.Shared/ExtensionMethod/Entity/BlockReferenceEx.cs
@@ -0,0 +1,143 @@
+﻿namespace IFoxCAD.Cad;
+
+/// <summary>
+/// 块参照扩展类
+/// </summary>
+public static class BlockReferenceEx
+{
+    #region 裁剪块参照
+
+    private const string filterDictName = "ACAD_FILTER";
+    private const string spatialName = "SPATIAL";
+
+    /// <summary>
+    /// 裁剪块参照
+    /// </summary>
+    /// <param name="bref">块参照</param>
+    /// <param name="pt3ds">裁剪多边形点表</param>
+    public static void ClipBlockRef(this BlockReference bref, IEnumerable<Point3d> pt3ds)
+    {
+        Matrix3d mat = bref.BlockTransform.Inverse();
+        var pts =
+            pt3ds
+            .Select(p => p.TransformBy(mat).Point2d())
+            .ToCollection();
+
+        SpatialFilterDefinition sfd = new(pts, Vector3d.ZAxis, 0.0, 0.0, 0.0, true);
+        using SpatialFilter sf = new() { Definition = sfd };
+        var dict = bref.GetXDictionary()!.GetSubDictionary(true, new string[] { filterDictName })!;
+        dict.SetAt<SpatialFilter>(spatialName, sf);
+        // SetToDictionary(dict, spatialName, sf);
+    }
+
+    /// <summary>
+    /// 裁剪块参照
+    /// </summary>
+    /// <param name="bref">块参照</param>
+    /// <param name="pt1">第一角点</param>
+    /// <param name="pt2">第二角点</param>
+    public static void ClipBlockRef(this BlockReference bref, Point3d pt1, Point3d pt2)
+    {
+        Matrix3d mat = bref.BlockTransform.Inverse();
+        pt1 = pt1.TransformBy(mat);
+        pt2 = pt2.TransformBy(mat);
+
+        Point2dCollection pts = new()
+        {
+            new Point2d(Math.Min(pt1.X, pt2.X), Math.Min(pt1.Y, pt2.Y)),
+            new Point2d(Math.Max(pt1.X, pt2.X), Math.Max(pt1.Y, pt2.Y))
+        };
+
+        using SpatialFilter sf = new()
+        {
+            Definition = new(pts, Vector3d.ZAxis, 0.0, 0.0, 0.0, true)
+        };
+        var dict = bref.GetXDictionary()!
+                       .GetSubDictionary(true, new string[] { filterDictName })!;
+        dict.SetAt<SpatialFilter>(spatialName, sf);
+        // SetToDictionary(dict, spatialName, sf);
+#if !acad
+        pts.Dispose();
+#endif
+    }
+    #endregion
+
+    #region 属性
+    /// <summary>
+    /// 更新动态块属性值
+    /// </summary>
+    /// <param name="blockReference">动态块</param>
+    /// <param name="propertyNameValues">属性值字典</param>
+    public static void ChangeBlockProperty(this BlockReference blockReference,
+                                           Dictionary<string, object> propertyNameValues)
+    {
+        if (!blockReference.IsDynamicBlock)
+            return;
+
+        using (blockReference.ForWrite())
+        {
+            foreach (DynamicBlockReferenceProperty item in blockReference.DynamicBlockReferencePropertyCollection)
+                if (propertyNameValues.ContainsKey(item.PropertyName))
+                    item.Value = propertyNameValues[item.PropertyName];
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// 遍历块内
+    /// </summary>
+    /// <param name="brf"></param>
+    /// <param name="action"></param>
+    /// <param name="tr"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    [System.Diagnostics.DebuggerStepThrough]
+    public static void ForEach(this BlockReference brf, Action<ObjectId> action, DBTrans? tr = null)
+    {
+        if (action == null)
+            throw new ArgumentNullException(nameof(action));
+
+        tr ??= DBTrans.Top;
+        var btr = tr.GetObject<BlockTableRecord>(brf.BlockTableRecord);
+        if (btr == null)
+            return;
+        btr.ForEach(action);
+    }
+    /// <summary>
+    /// 遍历块内
+    /// </summary>
+    /// <param name="brf"></param>
+    /// <param name="action"></param>
+    /// <param name="tr"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    [System.Diagnostics.DebuggerStepThrough]
+    public static void ForEach(this BlockReference brf, Action<ObjectId, LoopState> action, DBTrans? tr = null)
+    {
+        if (action == null)
+            throw new ArgumentNullException(nameof(action));
+
+        tr ??= DBTrans.Top;
+        var btr = tr.GetObject<BlockTableRecord>(brf.BlockTableRecord);
+        if (btr == null)
+            return;
+        btr.ForEach(action);
+    }
+    /// <summary>
+    /// 遍历块内
+    /// </summary>
+    /// <param name="brf"></param>
+    /// <param name="action"></param>
+    /// <param name="tr"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    [System.Diagnostics.DebuggerStepThrough]
+    public static void ForEach(this BlockReference brf, Action<ObjectId, LoopState, int> action, DBTrans? tr = null)
+    {
+        if (action == null)
+            throw new ArgumentNullException(nameof(action));
+
+        tr ??= DBTrans.Top;
+        var btr = tr.GetObject<BlockTableRecord>(brf.BlockTableRecord);
+        if (btr == null)
+            return;
+        btr.ForEach(action);
+    }
+}
