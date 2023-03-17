70fabe69f3a5437e4c68543b87e8ef3e71aabf30
DYH <1742647821@qq.com>
2023-03-17 18:45:01 +0800

    添加无类型匹配的GetObject函数

--- src/CAD/IFox.CAD.Shared/Runtime/DBTrans.cs
+++ src/CAD/IFox.CAD.Shared/Runtime/DBTrans.cs
@@ -390,6 +390,21 @@ public class DBTrans : IDisposable
 
     #region 获取对象
     /// <summary>
+    /// 根据对象id获取对象
+    /// </summary>
+    /// <param name="id">对象id</param>
+    /// <param name="openMode">打开模式,默认为只读</param>
+    /// <param name="openErased">是否打开已删除对象,默认为不打开</param>
+    /// <param name="openLockedLayer">是否打开锁定图层对象,默认为不打开</param>
+    /// <returns>数据库DBObject对象</returns>
+    public DBObject GetObject(ObjectId id,
+        OpenMode openMode = OpenMode.ForRead,
+        bool openErased = false,
+        bool openLockedLayer = false)
+    {
+        return Transaction.GetObject(id, openMode, openErased, openLockedLayer);
+    }
+    /// <summary>
     /// 根据对象id获取图元对象
     /// </summary>
     /// <typeparam name="T">要获取的图元对象的类型</typeparam>
