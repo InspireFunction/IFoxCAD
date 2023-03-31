2c69c355f117d3f271122f2a4c730cd873262401
vicwjb <vicwjb@163.com>
2023-04-01 00:05:04 +0800

    测试后台添加文字

--- tests/TestShared/TestText.cs
+++ tests/TestShared/TestText.cs
@@ -17,6 +17,22 @@ public class TestText
         });
     }
 
+    [CommandMethod(nameof(TestBackDBText))]
+    public void TestBackDBText()
+    {
+        using var tr = new DBTrans(@"C:\Users\vic\Desktop\test.dwg");
+        tr.CurrentSpace.AddDBText(new(-1, -1, 0), "123", 2.5, t => t.ColorIndex = 1);
+
+        tr.CurrentSpace.AddDBText(new(0, 0, 0), "123", 2.5, t => {
+            t.Justify = AttachmentPoint.BaseCenter;
+            t.AlignmentPoint = new(1, 1, 0);
+            t.ColorIndex = 2;
+        });
+        tr.SaveDwgFile();
+    }
+
+
+
     [CommandMethod(nameof(TestMText))]
     public void TestMText()
     {
