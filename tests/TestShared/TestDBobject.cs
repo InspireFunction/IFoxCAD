457b8b86929a0e829d6358f2c843a728cb75a955
vicwjb <vicwjb@163.com>
2023-04-20 23:45:45 +0800

    测试提权函数的速度，using模式比委托模式快一倍

---
+++ tests/TestShared/TestDBobject.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace TestShared
+{
+    public static class TestDBobject
+    {
+        [CommandMethod(nameof(TestForWrite))]
+        public static void TestForWrite()
+        {
+            using var tr = new DBTrans();
+            var ent = Env.Editor.GetEntity("\npick entity");
+            if (ent.Status is not PromptStatus.OK) return;
+            var entid = ent.ObjectId.GetObject<Entity>()!;
+            Tools.TestTimes3(100000, "using:", i => {
+                using (entid.ForWrite())
+                {
+                    entid.ColorIndex = i % 7;
+                }
+            });
+            Tools.TestTimes3(100000, "action:", i => {
+                entid.ForWrite(e => {
+                    e.ColorIndex = i % 7;
+                });
+            });
+        }
+    }
+}
