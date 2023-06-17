683422548a803c8e49a5b4dac0a3b2b1cf5d52b0
vicwjb <vicwjb@163.com>
2023-06-18 00:24:52 +0800

    增加 ArgumentNullEx.ThrowIfNull() 函数，并将 NotNull 函数标记为过时，准备在 v0.8进行移除

--- src/Basal/IFox.Basal.Shared/General/ArgumentNullEx.cs
+++ src/Basal/IFox.Basal.Shared/General/ArgumentNullEx.cs
@@ -8,9 +8,33 @@
         /// <summary>
         /// 检查参数是否为 null
         /// </summary>
+        /// <param name="argument">参数</param>
+        /// <param name="paramName">参数名字</param>
+        public static void ThrowIfNull([NotNull] object? argument, 
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument is null)
+            {
+                Throw(paramName);
+            }
+        }
+
+        [DoesNotReturn]
+        private static void Throw(string? paramName) => throw new ArgumentNullException(paramName);
+
+
+
+
+
+
+
+        /// <summary>
+        /// 检查参数是否为 null
+        /// </summary>
         /// <param name="value">参数</param>
         /// <param name="valueExpression">参数为null时的提示信息</param>
         /// <exception cref="ArgumentNullException"></exception>
+        [Obsolete("请使用 ArgumentNullEx.ThrowIfNull(value);")]
         public static void NotNull([NotNull] this object? value, [CallerArgumentExpression(nameof(value))] string valueExpression = "")
         {
             _ = value ?? throw new ArgumentNullException(nameof(value), valueExpression);
