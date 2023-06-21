4b47d9395f08b836769687947beed1d51fd3d84b
vicwjb <403009819@qq.com>
2023-06-21 15:24:01 +0800

    取消nullable包的引用，在basal里直接提供相关的特性代码

---
+++ src/Basal/IFox.Basal.Shared/Nullable/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// 指示参数将为另一个参数传递的表达式捕获为字符串。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        /// <summary>
+        /// 初始化 CallerArgumentExpressionAttribute 类的新实例。
+        /// </summary>
+        /// <param name="parameterName"></param>
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+        /// <summary>
+        /// 获取其表达式应捕获为字符串的参数的名称。
+        /// </summary>
+        public string ParameterName { get; }
+    }
+}
