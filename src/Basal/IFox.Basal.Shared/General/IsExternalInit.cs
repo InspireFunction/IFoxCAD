fa15cb857a1f9afcbe78941daf7dcb47e248aa6d
vicwjb <vicwjb@163.com>
2023-08-05 22:19:00 +0800

    支持c#9.0的init特性和record

---
+++ src/Basal/IFox.Basal.Shared/General/IsExternalInit.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#if NET30_OR_GREATER
+    public
+#else
+    internal
+#endif
+        static class IsExternalInit
+    {
+    }
+}
