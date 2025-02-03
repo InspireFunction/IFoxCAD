38a4f325dc773ab316afba0e1d3afac55f37af8a
vicwjb <vicwjb@163.com>
2025-02-03 23:32:57 +0800

    修正条件编译符号，解决用户的编译符号与ifox的源码不符时编译报错的问题

--- src/CADShared/Basal/Nullable/CallerArgumentExpressionAttribute.cs
+++ src/CADShared/Basal/Nullable/CallerArgumentExpressionAttribute.cs
@@ -1,5 +1,5 @@
 ﻿
-#if a2024 || zcad
+#if !NET8_0_OR_GREATER
 namespace System.Runtime.CompilerServices;
 /// <summary>
 /// 指示参数将为另一个参数传递的表达式捕获为字符串。
--- src/CADShared/Basal/Win/WindowsAPI.cs
+++ src/CADShared/Basal/Win/WindowsAPI.cs
@@ -1,6 +1,6 @@
 ﻿#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
 #define Marshal
-#if a2024 || zcad
+#if !NET8_0_OR_GREATER
 using ArgumentNullException = IFoxCAD.Basal.ArgumentNullEx;
 #endif
 namespace IFoxCAD.Basal;
