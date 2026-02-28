aadc9c58f4002fbaecb37d66cc23e39be7993063
liuqihong <540762622@qq.com>
2026-02-28 23:03:11 +0800

    重定向独立

--- tests/TestShared/CmdINI.cs
+++ tests/TestShared/CmdINI.cs
@@ -76,60 +76,6 @@ public class CmdInit
         }
     }
 }
-
-
-/// <summary>
-/// 重定向 Console
-/// </summary>
-public class ConsoleToEditorRedirector : TextWriter
-{
-    /// <summary>
-    /// 重定向 Console
-    /// </summary>
-    public ConsoleToEditorRedirector(Document doc)
-    {
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public override Encoding Encoding => Encoding.UTF8;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public override void WriteLine(string value)
-    {
-        var doc = Application.DocumentManager.MdiActiveDocument;
-        doc?.Editor?.WriteMessage($"\n{value}");
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public override void Write(string value)
-    {
-
-        var doc = Application.DocumentManager.MdiActiveDocument;
-        doc?.Editor?.WriteMessage(value);
-    }
-}
-
-public class Cmd
-{
-    /// <summary>
-    /// 重定向Console命令
-    /// </summary>
-    /// <param name="doc"></param>
-    [IFoxInitialize]
-    public void RedirectConsole(Document doc)
-    {
-        Console.SetOut(new ConsoleToEditorRedirector(doc));
-        Console.WriteLine("现在 Console.WriteLine 会输出到编辑器！\r");
-    }
-}
-
-
 /*
  * 自动执行:特性
  */
